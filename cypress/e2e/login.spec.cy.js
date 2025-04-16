e19110360d9ade8144eb7c57655c00fc77891f78
Douglas166 <douglassf0106@gmail.com>
2025-04-16 01:23:11 -0300

    userdata added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -6,17 +6,33 @@ describe('Orange HRM Tests', () => {
      tittleTopBar: "header",
      wrongCredendialAlert: "body",
    }
+   const userData = {
+    userSucess: {
+      username: "Admin"
+      password: "admin123"
+    }
+   userFail: {
+    username: "test"
+    password: "test"
+   }
+
+   }
+
+
+
+
+
   it('login - sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorslist.usarnameField).type('Admin')
-    cy.get(selectorslist.passwordField).type('admin123')
+    cy.get(selectorslist.usarnameField).type(userData.userSucess.username)
+    cy.get(selectorslist.passwordField).type(userData.userSucess.password)
     cy.get(selectorslist.loginButton).click()
     cy.get(selectorslist.tittleTopBar)
   })
 it('login - fail', () => {
   cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-  cy.get(selectorslist.usarnameField).type('Admin')
-  cy.get(selectorslist.passwordField).type('admin13')
+  cy.get(selectorslist.usarnameField).type(userData.userFail.username)
+  cy.get(selectorslist.passwordField).type(userData.userFail.password)
   cy.get(selectorslist.loginButton).click()
   cy.get(selectorslist.wrongCredendialAlert)
 })
