7fb36323aa6ec70dbd57016d2afb241a423980a0
Douglas166 <douglassf0106@gmail.com>
2025-04-18 22:34:23 -0300

    baseUrl added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,3 +1,5 @@
+import userData from '../fixtures/userdata.json'
+
 describe('Orange HRM Tests', () => {
    const selectorslist = {
      usarnameField: "[name='username']",
@@ -6,31 +8,16 @@ describe('Orange HRM Tests', () => {
      tittleTopBar: "header",
      wrongCredendialAlert: "body",
    }
-   const userData = {
-    userSucess: {
-      username: "Admin"
-      password: "admin123"
-    }
-   userFail: {
-    username: "test"
-    password: "test"
-   }
-
-   }
-
-
-
-
-
+   
   it('login - sucess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorslist.usarnameField).type(userData.userSucess.username)
     cy.get(selectorslist.passwordField).type(userData.userSucess.password)
     cy.get(selectorslist.loginButton).click()
     cy.get(selectorslist.tittleTopBar)
   })
 it('login - fail', () => {
-  cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+  cy.visit('/auth/login')
   cy.get(selectorslist.usarnameField).type(userData.userFail.username)
   cy.get(selectorslist.passwordField).type(userData.userFail.password)
   cy.get(selectorslist.loginButton).click()
