358c9dd4551a419bc9321606ceff81627e657bc4
Douglas166 <douglassf0106@gmail.com>
2025-04-21 16:20:16 -0300

    page objects added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,8 +1,11 @@
 import userData from '../fixtures/userdata.json'
+import LoginPage from '../pages/loginPage.js'
+
+const loginPage = new LoginPage()
 
 describe('Orange HRM Tests', () => {
    const selectorslist = {
-     usarnameField: "[name='username']",
+     usernameField: "[name='username']",
      passwordField: "[name='password']",
      loginButton: "button",
      tittleTopBar: "header",
@@ -18,40 +21,33 @@ describe('Orange HRM Tests', () => {
      closeButton: ".oxd-toast-close",
      selectButtonGeneric: "[clear='false']",
      nacionalityButton: ":nth-child(4) > span",
-     maritalStatusButton: ".oxd-select-dropdown > :nth-child(3)"
+     maritalStatusButton: ".oxd-select-dropdown > :nth-child(3)",
+     genderButton: ".oxd-radio-wrapper"
      }
 
   it.only('User Info Update - Sucess', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorslist.usarnameField).type(userData.userSucess.username)
-    cy.get(selectorslist.passwordField).type(userData.userSucess.password)
-    cy.get(selectorslist.loginButton).click()
-    cy.get(selectorslist.tittleTopBar)
-    cy.get(selectorslist.myInfo).click()
-    cy.get(selectorslist.firstNameField).clear().type('First')
-    cy.get(selectorslist.middleNameField).clear().type('Middle')
-    cy.get(selectorslist.lastNameField).clear().type('Last')
-    cy.get(selectorslist.genericField).eq(4).clear().type('Employee')
-    cy.get(selectorslist.genericField).eq(5).clear().type('OtherIdTest')
-    cy.get(selectorslist.genericField).eq(6).clear().type('DriverTest')
-    cy.get(selectorslist.dateGenericField).eq(0).clear().type('2030-05-05')
-    cy.get(selectorslist.dateCloseButton).click()
-    cy.get(selectorslist.selectButtonGeneric).eq(0).click()
-    cy.get(selectorslist.nacionalityButton).click()
-    cy.get(selectorslist.selectButtonGeneric).eq(1).click()
-    cy.get(selectorslist.maritalStatusButton).click()
-    cy.get(selectorslist.submitButton).eq(0).click()
-    cy.get('body').should('contain', "Successfully Updated")
-    cy.get(selectorslist.closeButton)
-
-
+   loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+   cy.get(selectorslist.tittleTopBar).should('be.visible')
+   cy.get(selectorslist.myInfo).click()
+   cy.get(selectorslist.firstNameField).clear().type('firstname')
+   cy.get(selectorslist.middleNameField).clear().type('middlename') 
+   cy.get(selectorslist.lastNameField).clear().type('lastname')
+   cy.get(selectorslist.selectButtonGeneric).eq(0).click()
+   cy.get(selectorslist.nacionalityButton).click() 
+   cy.get(selectorslist.selectButtonGeneric).eq(1).click()
+   cy.get(selectorslist.maritalStatusButton).click()
+   cy.get(selectorslist.dateGenericField).eq(0).clear().type('2003-10-01')
+   cy.get(selectorslist.dateCloseButton).click()
+   cy.get(selectorslist.genderButton).eq(0).click()
+   cy.get(selectorslist.submitButton).eq(0).click()
+   cy.get(selectorslist.closeButton)
 })  
 
 it('login - fail', () => {
   cy.visit('/auth/login')
-  cy.get(selectorslist.usarnameField).type(userData.userFail.username)
+  cy.get(selectorslist.usernameField).type(userData.userFail.username)
   cy.get(selectorslist.passwordField).type(userData.userFail.password)
   cy.get(selectorslist.loginButton).click()
-  cy.get(selectorslist.wrongCredendialAlert)
+  cy.get(selectorslist.usernameField).type(userData.userFail.username)
 })
 })
---
+++ cypress/pages/loginPage.js
@@ -0,0 +1,23 @@
+
+class LoginPage {
+  selectors = {
+      usernameField: "[name='username']",
+      passwordField: "[name='password']",
+      loginButton: "button",
+      wrongCredendialAlert: "body",
+  };
+
+  acessLoginPage() {
+      cy.visit('/auth/login');
+  }
+
+  loginWithUser(username, password) {
+    cy.visit('/auth/login');
+      cy.get(this.selectors.usernameField).type(username);
+      cy.get(this.selectors.passwordField).type(password);
+      cy.get(this.selectors.loginButton).click();
+      cy.get(this.selectors.wrongCredendialAlert).should('not.contain', 'Invalid credentials');
+  }
+}
+
+export default LoginPage;
